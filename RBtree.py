0e7e0aa122ed31431f6124c7ac39d5591e531469
markzjt <j.markham@live.com>
2025-10-27 11:12:03 -0700

    Fixed indentation of methods to be included in RedBlack Tree class. Implemented V1 of left rotation but have not tested integration

--- RBtree.py
+++ RBtree.py
@@ -20,49 +20,72 @@ class RedBlackTree:
         self.EMPTY = Node(None)
         self.EMPTY.color = Color.BLACK
         self.root = self.EMPTY
-
-# Required Methods For RBT
-
-# Insert Into Tree (BST)
-def insert(self, value):
-        new_node = Node(value)
-        new_node.left = self.EMPTY
-        new_node.right = self.EMPTY
         
-        parent = None
-        current = self.root
-        
-        while current != self.EMPTY:
-            parent = current
-            if new_node.value < current.value:
-                current = current.left
+    # Insert Into Tree (BST)
+    def insert(self, value):
+            new_node = Node(value)
+            new_node.left = self.EMPTY
+            new_node.right = self.EMPTY
+            
+            parent = None
+            current = self.root
+            
+            while current != self.EMPTY:
+                parent = current
+                if new_node.value < current.value:
+                    current = current.left
+                else:
+                    current = current.right
+            
+            new_node.parent = parent
+            
+            if parent is None:
+                self.root = new_node
+            elif new_node.value < parent.value:
+                parent.left = new_node
             else:
-                current = current.right
-        
-        new_node.parent = parent
-        
-        if parent is None:
-            self.root = new_node
-        elif new_node.value < parent.value:
-            parent.left = new_node
-        else:
-            parent.right = new_node
-        
-        new_node.color = Color.RED
-        # We will need to call fix insert here, most likely.
+                parent.right = new_node
+            
+            new_node.color = Color.RED
+            # We will need to call fix insert here, most likely.
 
-# In-Order Tree Traversal
-def in_order_traversal(self, node=None):
-        if node is None:
-            node = self.root
+    # In-Order Tree Traversal
+    def in_order_traversal(self, node=None):
+            if node is None:
+                node = self.root
+            
+            if node != self.EMPTY:
+                self.in_order_traversal(node.left)
+                print(node.value)
+                self.in_order_traversal(node.right)
+    
+    # Rotate left
+    def _left_rotate(self, node):
+        # Declare new paretn node
+        new_parent = node.right
+    
+        # Move new parent's left subtree to location of node's right subtree
+        node.right = new_parent.left
+    
+        # Update parent after subtree moves
+        if new_parent.left != self.EMPTY:
+            new_parent.left.parent = node
+    
+        # Update the parent of the new parent
+        new_parent.parent = node.parent
+    
+        if node.parent is None:
+            self.root = new_parent
+        elif node == node.parent.left:
+            node.parent.left = new_parent
+        else: 
+            node.parent.right = new_parent
+        # Make node the left child of the new parent
+        new_parent.left = node
+        node.parent = new_parent
         
-        if node != self.EMPTY:
-            self.in_order_traversal(node.left)
-            print(node.value)
-            self.in_order_traversal(node.right)
 
+# rotate right
     # Search tree for value
-    # rotate left
-    # rotate right
     # fix after insertion
     
