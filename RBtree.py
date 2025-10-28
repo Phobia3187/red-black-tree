ebdfd2c5bc19da0a223c3049141c97e8563b9ae0
Phobia3187 <kalumlfischer@gmail.com>
2025-10-27 19:13:06 -0700

    Search Tree

--- RBtree.py
+++ RBtree.py
@@ -10,82 +10,93 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.color = Color.RED
-        self.left: 'Node' = None
-        self.right: 'Node' = None
-        self.parent: 'Node' = None
+        self.left: "Node" = None
+        self.right: "Node" = None
+        self.parent: "Node" = None
 
-# Implement RB Tree    
+# Implement RB Tree
 class RedBlackTree:
     def __init__(self):
         self.EMPTY = Node(None)
         self.EMPTY.color = Color.BLACK
         self.root = self.EMPTY
-        
+
     # Insert Into Tree (BST)
     def insert(self, value):
-            new_node = Node(value)
-            new_node.left = self.EMPTY
-            new_node.right = self.EMPTY
-            
-            parent = None
-            current = self.root
-            
-            while current != self.EMPTY:
-                parent = current
-                if new_node.value < current.value:
-                    current = current.left
-                else:
-                    current = current.right
-            
-            new_node.parent = parent
-            
-            if parent is None:
-                self.root = new_node
-            elif new_node.value < parent.value:
-                parent.left = new_node
+        new_node = Node(value)
+        new_node.left = self.EMPTY
+        new_node.right = self.EMPTY
+
+        parent = None
+        current = self.root
+
+        while current != self.EMPTY:
+            parent = current
+            if new_node.value < current.value:
+                current = current.left
             else:
-                parent.right = new_node
-            
-            new_node.color = Color.RED
-            # We will need to call fix insert here, most likely.
+                current = current.right
+
+        new_node.parent = parent
+
+        if parent is None:
+            self.root = new_node
+        elif new_node.value < parent.value:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+        new_node.color = Color.RED
+        # We will need to call fix insert here, most likely.
 
     # In-Order Tree Traversal
     def in_order_traversal(self, node=None):
-            if node is None:
-                node = self.root
-            
-            if node != self.EMPTY:
-                self.in_order_traversal(node.left)
-                print(node.value)
-                self.in_order_traversal(node.right)
-    
+        if node is None:
+            node = self.root
+
+        if node != self.EMPTY:
+            self.in_order_traversal(node.left)
+            print(node.value)
+            self.in_order_traversal(node.right)
+
     # Rotate left
     def _left_rotate(self, node):
         # Declare new paretn node
         new_parent = node.right
-    
+
         # Move new parent's left subtree to location of node's right subtree
         node.right = new_parent.left
-    
+
         # Update parent after subtree moves
         if new_parent.left != self.EMPTY:
             new_parent.left.parent = node
-    
+
         # Update the parent of the new parent
         new_parent.parent = node.parent
-    
+
         if node.parent is None:
             self.root = new_parent
         elif node == node.parent.left:
             node.parent.left = new_parent
-        else: 
+        else:
             node.parent.right = new_parent
         # Make node the left child of the new parent
         new_parent.left = node
         node.parent = new_parent
+
+    # Search Tree
+    def search(self, value):
+        current = self.root
         
+        while current != self.EMPTY:
+            if value == current.value:
+                return current
+            elif value < current.value:
+                current = current.left
+            else:
+                current = current.right
+                
+        return None
 
 # rotate right
-    # Search tree for value
-    # fix after insertion
-    
+# fix after insertion
